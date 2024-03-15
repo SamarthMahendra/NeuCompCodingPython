7a1217eb1c9d918b975ed91bc9bdc09cc74a9467
Samarth Mahendra <samarth.mahendragowda@gmail.com>
2024-03-15 16:36:18 -0400

    anagram

---
+++ Anagram.py
@@ -0,0 +1,45 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        d = {}
+        for i in s:
+            if i in d:
+                d[i] = d[i] + 1
+            else:
+                d[i] = 1
+
+        d2 = {}
+        for i in t:
+            if i in d2:
+                d2[i] =d2[i] +1
+            else:
+                d2[i] = 1
+        for i, v in d.items():
+            if i not in d2:
+                return False
+            if v != d2[i]:
+                return False
+        for i, v in d2.items():
+            if i not in d:
+                return False
+            if v != d[i]:
+                return False
+        return True
+
+
+    def isAnagram2(self, x: str, t: str) -> bool:
+        s= list(t)
+        j=0
+        for i in range(len(x)):
+                try:
+                    s.remove(x[i])
+                    j=j+1
+                except:
+                    continue
+        if s or j<len(x):
+            return False
+        else:
+            return True
+
+
+obj = Solution()
+print(obj.isAnagram2("anagram", "nagaram"))
---
+++ GroupAnagram.py
@@ -0,0 +1,45 @@
+from collections import defaultdict
+
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        res = {}
+        for i in range(len(strs)):
+            d1 = self.charFrequency(strs[i])
+            d1 = "".join(d1)
+            if d1 in res:
+                res[d1].append(strs[i])
+            else:
+                res[d1] = [strs[i]]
+        r = []
+        for k,v in res.items():
+            r.append(v)
+        return r
+
+
+    @staticmethod
+    def charFrequency(st):
+        # list containing 26 0's
+        d = [''] * 26
+        x  = []
+        for i in st:
+            if i in x:
+                d[ord(i)-ord('a')] = str(int(d[ord(i)-ord('a')]) + 1)
+            else:
+                x.append(i)
+                d[ord(i)-ord('a')] = '1'
+        for j in range(len(d)):
+            d[j] = chr(100+j) + d[j]
+        return d
+
+    def groupAnagrams(self, strs):
+        res = defaultdict(list)
+        for i in strs:
+            sorted_str = sorted(i)
+            s = "".join(sorted_str)
+            res[s].append(i)
+        return list(res.values())
+
+
+obj = Solution()
+print(obj.groupAnagrams(["abbbbbbbbbbb","aaaaaaaaaaab"]))
