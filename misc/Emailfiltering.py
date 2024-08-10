eac2a6e70722cf67fd465fb969c4c9cc0bca7fa7
M Samarth Mahendra <39624187+SamarthMahendra@users.noreply.github.com>
2024-08-10 02:28:15 -0400

    email processing

---
+++ misc/Emailfiltering.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        res = set()
+        for email in emails:
+            local, domain = email.split('@')
+            local = local.split('+')[0]  # Take the part before the '+'
+            local = local.replace('.', '')  # Remove all dots
+            res.add(f"{local}@{domain}")
+        return len(res)
