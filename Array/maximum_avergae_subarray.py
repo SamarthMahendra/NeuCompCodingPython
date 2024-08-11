095f19875f9803a94722000e377c2c73d20fed66
M Samarth Mahendra <39624187+SamarthMahendra@users.noreply.github.com>
2024-08-11 15:08:03 -0400

    maximum averge subarray problem

---
+++ Array/maximum_avergae_subarray.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        maxsum = sum(nums[0:k])
+        cur = maxsum
+        for i in range(k, len(nums)):
+            cur = cur + nums[i] - nums[i - k]
+            maxsum = max(maxsum, cur)
+        return maxsum / k
